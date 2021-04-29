db1d2957e60266417eaa9784f33a688487966d60
Pavel Belevich <pbelevich@fb.com>
2021-04-28 18:27:56 -0700

    NEW CUDA RPC MNIST experiment
    [ghstack-poisoned]

---
+++ NEW/cuda_rpc2.py
@@ -0,0 +1,106 @@
+import os
+import socket
+import subprocess
+
+import torch.distributed.rpc as rpc
+import torch.nn as nn
+from torch.distributed.rpc import RRef
+
+
+class DistributedCUDARPCSequential(nn.Module):
+    def __init__(self, *worker_layers):
+        super().__init__()
+        self.worker_layers = worker_layers
+
+    def forward(self, x):  # x is cpu tensor on master
+        x_rref = RRef(x)  # x_rref is initially on master cpu
+        for worker_layer in self.worker_layers:
+            x_rref = worker_layer(x_rref)  # pass to worker layer
+        return x_rref.to_here()  # get x to master cpu
+
+    def train(self, mode=True):
+        for worker_layer in self.worker_layers:
+            worker_layer.train(mode=mode)
+
+    def eval(self):
+        self.train(mode=False)
+
+    def parameter_rrefs(self):
+        remote_params = []
+        for worker_layer in self.worker_layers:
+            remote_params.extend(worker_layer.parameter_rrefs().to_here())
+        return remote_params
+
+
+class WorkerModule(nn.Module):
+    def __init__(self, worker, remote_class_creator, *args, **kwargs):
+        super().__init__()
+        self.remote_module = rpc.remote(worker, LocalWrapper, (remote_class_creator, *args), kwargs)
+
+    def train(self, mode=True):
+        self.remote_module.rpc_sync().train(mode=mode)
+
+    def forward(self, x_rref):
+        return self.remote_module.remote().forward(x_rref)
+
+    def parameter_rrefs(self):
+        return self.remote_module.remote().parameter_rrefs()
+
+
+class LocalWrapper(nn.Module):
+    def __init__(self, local_net_creator, *args, **kwargs):
+        super().__init__()
+        self.local_net = local_net_creator(*args, **kwargs)
+        first_parameter = next(self.local_net.parameters(), None)
+        self.first_device = local_net_creator.device
+
+    def train(self, mode=True):
+        self.local_net.train(mode=mode)
+
+    def forward(self, x_rref):
+        x = x_rref.to_here()
+        return self.local_net(x)
+
+    def parameter_rrefs(self):
+        return [RRef(p) for p in self.local_net.parameters()]
+
+
+def get_my_gpu_index():
+    try:
+        return int(os.getenv("CUDA_VISIBLE_DEVICES"))
+    except:
+        try:
+            p1 = subprocess.Popen(["nvidia-smi"], stdout=subprocess.PIPE)
+            p2 = subprocess.Popen(["grep", str(os.getpid())], stdin=p1.stdout, stdout=subprocess.PIPE)
+            p3 = subprocess.Popen(["awk", "{print $2}"], stdin=p2.stdout, stdout=subprocess.PIPE)
+            return int(p3.communicate()[0])
+        except:
+            return None
+
+
+class _layer_on_device_helper():
+    def __init__(self, device):
+        self.device = device
+
+    def __call__(self, layer_class, *args, **kwargs):
+        res = layer_class(*args, **kwargs).to(self.device)
+        print(f"Materializing {layer_class} on {socket.gethostname()}:{get_my_gpu_index()}")
+        return res
+
+
+def layer_on_device(device):
+    return _layer_on_device_helper(device)
+
+
+class _pipeline_on_devices_helper():
+    def __init__(self, *devices, **config):
+        self.devices = devices
+        self.device = devices[0]
+        self.config = config
+
+    def __call__(self, pipeline_class, *args, **kwargs):
+        return pipeline_class(self.devices, self.config, *args, **kwargs)
+
+
+def pipeline_on_devices(*devices, **kwargs):
+    return _pipeline_on_devices_helper(*devices, **kwargs)
---
+++ NEW/mnist2_cuda.py
@@ -0,0 +1,141 @@
+import argparse
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import datasets, transforms
+import torch.multiprocessing as mp
+import torch.distributed.rpc as rpc
+import os
+import concurrent.futures
+from torch.distributed.optim import DistributedOptimizer
+from torch.distributed.rpc import RRef
+import torch.distributed.autograd as dist_autograd
+from tqdm import tqdm
+from cuda_rpc2 import DistributedCUDARPCSequential, WorkerModule, layer_on_device, pipeline_on_devices
+
+
+def run_main(world_size):
+    batch_size = 100
+
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,))
+    ])
+
+    train_data = datasets.MNIST('./data', train=True, download=True, transform=transform)
+    valid_data = datasets.MNIST('./data', train=False, transform=transform)
+
+    train_dataloader = torch.utils.data.DataLoader(train_data, batch_size=batch_size)
+    valid_dataloader = torch.utils.data.DataLoader(valid_data, batch_size=batch_size)
+
+    model = DistributedCUDARPCSequential(
+        WorkerModule("worker1", layer_on_device("cuda:0"), nn.Flatten),
+        WorkerModule("worker2", layer_on_device("cuda:1"), nn.Linear, 28 * 28, 128),
+        WorkerModule("worker3", layer_on_device("cuda:2"), nn.ReLU),
+        WorkerModule("worker4", layer_on_device("cuda:3"), nn.Linear, 128, 64),
+        WorkerModule("worker5", layer_on_device("cuda:4"), nn.ReLU),
+        WorkerModule("worker6", layer_on_device("cuda:5"), nn.Linear, 64, 10),
+    )
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = DistributedOptimizer(
+        optim.Adam,
+        model.parameter_rrefs(),
+    )
+
+    loaders = {"train": train_dataloader, "valid": valid_dataloader}
+
+    max_epochs = 10
+    accuracy = {"train": [], "valid": []}
+    for epoch in range(max_epochs):
+        print(f"Epoch: {epoch+1}")
+        epoch_correct = 0
+        epoch_all = 0
+        for k, dataloader in loaders.items():
+            for i, (x_batch, y_batch) in enumerate(tqdm(dataloader)):
+                x_batch = x_batch.to(0)
+                y_batch = y_batch.to(5)
+                if k == "train":
+                    model.train()
+                    with dist_autograd.context() as context_id:
+                        outp = model(x_batch)
+                        preds = outp.argmax(-1)
+                        correct = (preds == y_batch).sum()
+                        all = len(y_batch)
+                        epoch_correct += correct.item()
+                        epoch_all += all
+                        loss = criterion(outp, y_batch)
+                        dist_autograd.backward(context_id, [loss])
+                        optimizer.step(context_id)
+                else:
+                    model.eval()
+                    with torch.no_grad():
+                        outp = model(x_batch)
+                        preds = outp.argmax(-1)
+                        correct = (preds == y_batch).sum()
+                        all = len(y_batch)
+                        epoch_correct += correct.item()
+                        epoch_all += all
+                                  
+            print(f"Loader: {k}. Accuracy: {epoch_correct/epoch_all}")
+            accuracy[k].append(epoch_correct/epoch_all)
+
+
+def run_worker(rank, world_size, args):
+    os.environ['MASTER_ADDR'] = args.master_addr
+    os.environ['MASTER_PORT'] = args.master_port
+    options = rpc.TensorPipeRpcBackendOptions(num_worker_threads=256)
+
+    if rank == 0:
+        options.set_device_map("worker1", {0:0})
+        options.set_device_map("worker2", {1:1})
+        options.set_device_map("worker3", {2:2})
+        options.set_device_map("worker4", {3:3})
+        options.set_device_map("worker5", {4:4})
+        options.set_device_map("worker6", {5:5})
+
+        rpc.init_rpc(
+            "master",
+            rank=rank,
+            world_size=world_size,
+            rpc_backend_options=options
+        )
+        run_main(world_size)
+    else:
+        if rank == 1:
+            options.set_device_map("master", {0:0})
+        elif rank == 2:
+            options.set_device_map("worker1", {1:0})
+        elif rank == 3:
+            options.set_device_map("worker2", {2:1})
+        elif rank == 4:
+            options.set_device_map("worker3", {3:2})
+        elif rank == 5:
+            options.set_device_map("worker4", {4:3})
+        elif rank == 6:
+            options.set_device_map("worker5", {5:4})
+    
+        rpc.init_rpc(
+            f"worker{rank}",
+            rank=rank,
+            world_size=world_size,
+            rpc_backend_options=options
+        )
+
+    rpc.shutdown()
+
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(description='Pipeline experiments')
+
+    parser.add_argument('--world_size', type=int, default=None,
+                        help='the world size')
+    parser.add_argument('--rank', type=int, default=None,
+                        help="Global rank of this process. Pass in 0 for master.")
+    parser.add_argument('--master_addr', type=str, default='localhost',
+                        help="""Address of master, will default to localhost if not provided. Master must be able to accept network traffic on the address + port.""")
+    parser.add_argument('--master_port', type=str, default='29500',
+                        help="""Port that master is listening on, will default to 29500 if not provided. Master must be able to accept network traffic on the host and port.""")
+
+    args = parser.parse_args()
+    run_worker(args.rank, args.world_size, args)
