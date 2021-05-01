2a744cfc5e1d3f336f91a7cea1415c9b14aaf389
Pavel Belevich <belevichp@gmail.com>
2021-05-01 18:42:43 +0000

    BERT with 7 nodes 28 tasks --nlayers=96 --emsize=4096 --nhid=16384 --nhead=16
    [ghstack-poisoned]

--- BERT/bert_cuda_forward_tensor_interactive.sh
+++ BERT/bert_cuda_forward_tensor_interactive.sh
@@ -4,9 +4,9 @@ export USE_TQDM=1
 
 srun --label \
 	--job-name=bert_cuda_forward_tensor_interactive \
-	--ntasks=16 \
+	--ntasks=28 \
 	--partition=q2 \
-	--nodes=4 \
+	--nodes=7 \
 	--gpus-per-node=4 \
 	--gpus-per-task=1 \
 	--time=60:00 \
