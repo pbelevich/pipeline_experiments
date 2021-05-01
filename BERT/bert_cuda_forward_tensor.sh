fa888a06fa8d8dda90e35dcfcc2fe64af046e16f
Pavel Belevich <belevichp@gmail.com>
2021-05-01 04:25:45 +0000

    BERT with 16 nodes --nlayers=48 --emsize=4096 --nhid=16384 --nhead=16
    ghstack-source-id: bb633c98a8019e4b2a0b8a196fa3bb868628204c
    Pull Request resolved: https://github.com/pbelevich/pipeline_experiments/pull/12

--- BERT/bert_cuda_forward_tensor.sh
+++ BERT/bert_cuda_forward_tensor.sh
@@ -5,6 +5,10 @@ export MASTER_ADDR=$(scontrol show hostname ${SLURM_NODELIST} | head -n 1)
 export CUDA_VISIBLE_DEVICES=${SLURM_LOCALID}
 
 python -u bert_cuda_forward_tensor.py \
+	--nlayers=48 \
+	--emsize=4096 \
+	--nhid=16384 \
+	--nhead=16 \
         --world_size=${SLURM_NTASKS} \
         --rank=${SLURM_PROCID} \
         --master_addr=${MASTER_ADDR} \
