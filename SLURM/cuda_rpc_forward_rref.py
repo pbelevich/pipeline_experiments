467dc5d2ae7e20e4a900fbfa8461b08dcc38a323
Pavel Belevich <pbelevich@fb.com>
2021-05-04 10:44:30 -0400

    Parallel materializing and microbatching (#19)
    * Parallel materializing
    
    ghstack-source-id: f871ba6018993d47a94914f8808c3e231c5d7b09
    Pull Request resolved: https://github.com/pbelevich/pipeline_experiments/pull/17
    
    * Microbatching
    
    ghstack-source-id: d30881e3195fb9cddf56516edb9bee9cd95251a5
    Pull Request resolved: https://github.com/pbelevich/pipeline_experiments/pull/18

--- SLURM/cuda_rpc_forward_rref.py
+++ SLURM/cuda_rpc_forward_rref.py
@@ -1,22 +1,38 @@
 import os
 import socket
 import subprocess
+import concurrent.futures
+import threading
 
+import torch
 import torch.distributed.rpc as rpc
 import torch.nn as nn
 from torch.distributed.rpc import RRef
 
 
 class DistributedCUDARPCSequential(nn.Module):
-    def __init__(self, *worker_layers):
+    def __init__(self, *worker_layers, microbatch_size=None):
         super().__init__()
         self.worker_layers = worker_layers
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            concurrent.futures.wait([executor.submit(lambda wl: wl.materialize(), wl) for wl in self.worker_layers])
+        self.microbatch_size = microbatch_size
 
-    def forward(self, x):  # x is cpu tensor on master
-        x_rref = RRef(x)  # x_rref is initially on master cpu
-        for worker_layer in self.worker_layers:
-            x_rref = worker_layer(x_rref)  # pass to worker layer
-        return x_rref.to_here()  # get x to master cpu
+    def forward(self, xs):
+        if self.microbatch_size == None:
+            x_rref = RRef(xs)  # x_rref is initially on master cpu
+            for worker_layer in self.worker_layers:
+                x_rref = worker_layer(x_rref)  # pass to worker layer
+            return x_rref.to_here()  # get x to master cpu
+        else:
+            out_futures = []
+            for x in iter(xs.split(self.microbatch_size, dim=0)):
+                x_rref = RRef(x)
+                for worker_layer in self.worker_layers[:-1]:
+                    x_rref = worker_layer.forward(x_rref)
+                z_fut = self.worker_layers[-1].remote_module.rpc_async().forward(x_rref)
+                out_futures.append(z_fut)
+            return torch.cat(torch.futures.wait_all(out_futures))
 
     def train(self, mode=True):
         for worker_layer in self.worker_layers:
@@ -35,7 +51,14 @@ class DistributedCUDARPCSequential(nn.Module):
 class WorkerModule(nn.Module):
     def __init__(self, worker, remote_class_creator, *args, **kwargs):
         super().__init__()
-        self.remote_module = rpc.remote(worker, LocalWrapper, (remote_class_creator, *args), kwargs)
+        self.worker = worker
+        self.remote_class_creator = remote_class_creator
+        self.args = args
+        self.kwargs = kwargs
+        self.remote_module = None
+
+    def materialize(self):
+        self.remote_module = rpc.remote(self.worker, LocalWrapper, args=(self.remote_class_creator, *(self.args)), kwargs=self.kwargs)
 
     def train(self, mode=True):
         self.remote_module.rpc_sync().train(mode=mode)
@@ -53,13 +76,16 @@ class LocalWrapper(nn.Module):
         self.local_net = local_net_creator(*args, **kwargs)
         first_parameter = next(self.local_net.parameters(), None)
         self.first_device = local_net_creator.device
+        self._lock = threading.Lock()
 
     def train(self, mode=True):
         self.local_net.train(mode=mode)
 
     def forward(self, x_rref):
         x = x_rref.to_here()
-        return self.local_net(x)
+        with self._lock:
+            res = self.local_net(x)
+        return res
 
     def parameter_rrefs(self):
         return [RRef(p) for p in self.local_net.parameters()]
