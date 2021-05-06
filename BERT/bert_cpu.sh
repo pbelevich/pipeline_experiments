7be959d3ba9e8b280335dbc0bed1e726a7c78f4d
Pavel Belevich <belevichp@gmail.com>
2021-05-06 05:56:12 +0000

    Add UNUSED_GPUS
    ghstack-source-id: be93ae14d476e99038239f402623e87969328f42
    Pull Request resolved: https://github.com/pbelevich/pipeline_experiments/pull/24

--- BERT/bert_cpu.sh
+++ BERT/bert_cpu.sh
@@ -3,9 +3,10 @@
 export MASTER_PORT=29500
 export MASTER_ADDR=$(scontrol show hostname ${SLURM_NODELIST} | head -n 1)
 export CUDA_VISIBLE_DEVICES=${SLURM_LOCALID}
+export UNUSED_GPUS=1
 
 python -u bert_cpu.py \
-        --world_size=${SLURM_NTASKS} \
+        --world_size=$((SLURM_NTASKS-UNUSED_GPUS)) \
         --rank=${SLURM_PROCID} \
         --master_addr=${MASTER_ADDR} \
         --master_port=${MASTER_PORT}
