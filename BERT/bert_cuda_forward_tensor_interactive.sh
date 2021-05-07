5c62243eb7947840d80581bbd580b224140090c6
Pavel Belevich <belevichp@gmail.com>
2021-05-07 02:37:26 +0000

    q3:175B, --nlayers=96 --emsize=12288 --nhid=49152 --nhead=16 --batch_size=32
    ghstack-source-id: 50c3865ee3c1208b409ad03bd3ac966ffcd9f4ba
    Pull Request resolved: https://github.com/pbelevich/pipeline_experiments/pull/25

--- BERT/bert_cuda_forward_tensor_interactive.sh
+++ BERT/bert_cuda_forward_tensor_interactive.sh
@@ -4,10 +4,10 @@ export USE_TQDM=1
 
 srun --label \
 	--job-name=bert_cuda_forward_tensor_interactive \
-	--ntasks=8 \
-	--partition=q2 \
-	--nodes=4 \
-	--gpus-per-node=2 \
+	--ntasks=104 \
+	--partition=q3 \
+	--nodes=13 \
+	--gpus-per-node=8 \
 	--gpus-per-task=1 \
-	--time=30:00 \
+	--time=24:00:00 \
 	bert_cuda_forward_tensor.sh
