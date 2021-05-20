32d20f1b9a4192e75ed6ba709c9acd2e0cf23e06
Pavel Belevich <pbelevich@fb.com>
2021-05-20 07:46:28 -0700

    Local pipeline comp/comm measurements
    ghstack-source-id: 34a8636bf329a250b9f84bd147043768ba1f47b8
    Pull Request resolved: https://github.com/pbelevich/pipeline_experiments/pull/31

--- BERT/cuda_local_pipeline.py
+++ BERT/cuda_local_pipeline.py
@@ -4,13 +4,40 @@ import torch.nn as nn
 class LocalSequential(nn.Sequential):
     def __init__(self, *layers):
         super().__init__(*layers)
+        self.before_comm = torch.cuda.Event(enable_timing=True)
+        self.after_comm = torch.cuda.Event(enable_timing=True)
+        self.before_comp = torch.cuda.Event(enable_timing=True)
+        self.after_comp = torch.cuda.Event(enable_timing=True)
+        self.fwd_compute_delay = 0
+        self.fwd_communication_delay = 0
 
     def forward(self, x):
+        self.fwd_compute_delay = 0
+        self.fwd_communication_delay = 0
         for layer in self:
             device = next(layer.parameters()).device
+            sync_all_device(len(self))
+            self.before_comm.record()
             x = x.to(device)
+            sync_all_device(len(self))
+            self.after_comm.record()
+            self.before_comp.record()
             x = layer(x)
+            sync_all_device(len(self))
+            self.after_comp.record()
+
+            self.after_comm.synchronize()
+            self.fwd_communication_delay += self.before_comm.elapsed_time(self.after_comm)
+
+            self.after_comp.synchronize()
+            self.fwd_compute_delay += self.before_comp.elapsed_time(self.after_comp)
         return x
+
+    def get_fwd_communication_delay(self):
+        return self.fwd_communication_delay # * 1000
+
+    def get_fwd_compute_delay(self):
+        return self.fwd_compute_delay # * 1000
 
 
 # assuming CUDA_VISIBLE_DEVICES are configured in a way that each process only sees
